74b347eb68b51eff9384ef479dc6187c396bfe8b
Ahmed Mahdy <aybassiouny@gmail.com>
2019-02-01 01:11:49 -0800

    Still trying stuff, not working

--- RestServer/RestServer.cpp
+++ RestServer/RestServer.cpp
@@ -1,13 +1,9 @@
-// HelloSF.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
 #include <iostream>
 
 #include <cpprest/http_listener.h>
 #include <cpprest/json.h>
 
-#pragma comment(lib, "cpprest_2_10")
+#include <boost/asio/ip/host_name.hpp>
 
 using namespace web;
 using namespace web::http;
@@ -15,19 +11,32 @@ using namespace web::http::experimental::listener;
 
 std::map<utility::string_t, utility::string_t> dictionary;
 
-void handle_get(http_request request)
+std::string c_hostName;
+
+struct GetHandler
 {
-    std::cout<<"\nHandling a GET request\n";
-    request.reply(status_codes::OK, "Got a request!");
-}
+    GetHandler()
+    {
+        m_hostName  = boost::asio::ip::host_name();
+    }
+
+    void operator()(http_request request)
+    {
+        std::string msg = "Node " + m_hostName + " ready to serve!";
+        request.reply(status_codes::OK, msg);
+    }
+
+private:
+    std::string m_hostName;
+};
 
 int main()
 {
     std::cout << "\nStarting ...\n";
-    //http_listener listener(L"http://*:8888/restdemo");
-    http_listener listener(L"http://127.0.0.1:8888/restdemo");
+    http_listener listener(L"http://*:9009/processrequest");
 
-    listener.support(methods::GET, handle_get);
+    GetHandler getHandler;
+    listener.support(methods::GET, getHandler);
 
     try
     {
@@ -41,8 +50,9 @@ int main()
     catch (std::exception const & e)
     {
         std::cout << e.what() << std::endl;
+        return -1;
     }
 
     std::cout << "\nExiting ...\n";
-    return -1;
+    return 0;
 }
